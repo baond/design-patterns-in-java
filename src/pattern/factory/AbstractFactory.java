8710f22679161c83c3ebb785430c0313c1df3c35
bao_nguyendien <bao.nguyendien@ascendcorp.com>
2020-08-24 12:11:17 +0700

    abstract factory

---
+++ src/pattern/factory/AbstractFactory.java
@@ -0,0 +1,98 @@
+package pattern.factory;
+
+import com.sun.tools.javac.util.Pair;
+import org.reflections.Reflections;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class AbstractFactory {
+    public static void main(String[] args) throws Exception {
+        HotDrinkMachine machine = new HotDrinkMachine();
+        HotDrink drink = machine.makeDrink();
+        drink.consume();
+    }
+}
+
+interface HotDrink {
+    void consume();
+}
+
+class Tea implements HotDrink {
+
+    @Override
+    public void consume() {
+        System.out.println("This tee is delicious");
+    }
+}
+
+class Coffee implements HotDrink {
+    @Override
+    public void consume() {
+        System.out.println("This coffee is delicious");
+    }
+}
+
+interface HotDrinkFactory {
+    HotDrink prepare(int amount);
+}
+
+class TeaFactory implements HotDrinkFactory {
+
+    @Override
+    public HotDrink prepare(int amount) {
+        System.out.println("Put in tea bag, boil water, pour " + amount + " ml, add lemon, enjoy!");
+        return new Tea();
+    }
+}
+
+class CoffeeFactory implements HotDrinkFactory {
+
+    @Override
+    public HotDrink prepare(int amount) {
+        System.out.println("Grind some beans, boil water, pour " + amount + " ml. add cream and sugar, enjoy!");
+        return new Coffee();
+    }
+}
+
+class HotDrinkMachine {
+    private List<Pair<String, HotDrinkFactory>> namedFactories = new ArrayList<>();
+
+    public HotDrinkMachine() throws Exception {
+        Set<Class<? extends HotDrinkFactory>> types = new Reflections("").getSubTypesOf(HotDrinkFactory.class);
+        for(Class<? extends HotDrinkFactory> clazz : types){
+            Pair p = new Pair<>(clazz.getSimpleName().replace("Factory", ""),
+                    clazz.getDeclaredConstructor().newInstance());
+            namedFactories.add(p);
+        }
+    }
+
+    public HotDrink makeDrink() throws Exception {
+        System.out.println("Available drink: ");
+        for (int index = 0; index < namedFactories.size(); index++) {
+            Pair<String, HotDrinkFactory> item = namedFactories.get(index);
+            System.out.println("" + index + ": " + item.fst);
+        }
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            String s;
+            int i, amount;
+            if((s = reader.readLine()) != null
+            && (i = Integer.parseInt(s)) >= 0
+            && i < namedFactories.size()){
+                System.out.println("Specify amount: ");
+                s = reader.readLine();
+                if(s != null
+                && (amount = Integer.parseInt(s)) >= 0
+                && amount > 0){
+                    return namedFactories.get(i).snd.prepare(amount);
+                }
+            }
+            System.out.println("Incorrect input");
+        }
+    }
+}
